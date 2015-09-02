8f6d73dab4e7a31db663c3c2330f488bc7e01f71
eSlivinski <eslivinski@gmail.com>
2015-09-02 10:05:11 -0400

    selection dropdown for changing the map center

--- js/main.js
+++ js/main.js
@@ -4,25 +4,34 @@ var map,
 
 // More at http://leaflet-extras.github.io/leaflet-providers/preview/
 var basemapOptions = {
-	blackAndWhite 		: L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {}),
-	imagery 			: L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {}),
-	watercolor 			: L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',{}),
-	osm 				: L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {})
+	blackAndWhite : L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {}),
+	imagery : L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {}),
+	watercolor : L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',{}),
+	osm : L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {})
 };
 
-var buffalo = [42.8857, -78.8787],
-	madison = {lat: 43.08298, lng: -89.3791};
-
-var cities = [buffalo, madison]
+var cities = {
+	albany	: [42.6525, -73.7572],
+	buffalo : [42.8857, -78.8787],
+	madison : {lat: 43.08298, lng: -89.3791}
+};
 
 function createMap() {
 	map = L.map('map', {
-		center 	: cities[0],
-		layers 	: [basemapOptions.watercolor],
-		zoom	: 13
+		center 	:	cities['albany'],
+		layers  : 	[basemapOptions.osm],
+		zoom 	: 	13
 	});
 
 	L.control.layers(basemapOptions).addTo(map);
 }
 
-$(window).on('load', createMap);
+function changeCity(selectedCity){
+	// console.log(selectedCity + " was selected from the dropdown menu.");
+
+	var currentCity = cities[selectedCity]; // Access the coordinates of the selected city in the cities object using the city's name (the selectedCity var) as a key
+	map.panTo(currentCity);
+}
+
+
+$(window).on('load', createMap); // Calls the create map function when the page is loaded
