85d6172e7ba597fe246262e753e991277e66bd2b
eSlivinski <eslivinski@gmail.com>
2015-09-05 10:11:12 -0400

    styling

--- data/py/bigfootParser.py
+++ data/py/bigfootParser.py
@@ -11,27 +11,23 @@ def parse():
 
         for row in list(reader):
             try:
-                pairwise = zip(headings, row)
-                del pairwise[0]
-                del pairwise[3]
-                pairwise[1] = ('id', pairwise[1][1].strip('Report '))
-                lat = pairwise.pop(-1)
-                pairwise[-1] =('latlng', [float(lat[1]), float(pairwise[-1][1])])
-                x = dict(pairwise)
-
-                year = x['reportdate'].split('-')[0]
-                x['year'] = int(year)
+                x = dict(zip(headings, row))
+                del x['county']
+                del x['foldername']
+                x['yr']  = int(x['reportdate'].split('-')[0])
+                x['lat'] = float(x['lat'])
+                x['lng'] = float(x['lng'])
+                print str(x)+'\n'
                 sightings.append(x)
             except:
                 print('An Error Occured')
-        print(str(sightings))
         bigfoots.close()
 
     with open('/Users/lizzi/Documents/sites/intro-2-js/data/bigfoots_formatted.js', 'w') as bigfootsJS:
         bigfootsJS.write('var bigfoots = ' + str(sightings) + ';')
         bigfootsJS.close()
 
-    return "Task Complete\n-->Parsed " +str(len(sightings))+ " Records"
+    return "Task Complete\n--> Parsed " +str(len(sightings))+ " Records"
 
 
 def start():
--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ var map,
 
 // More at http://leaflet-extras.github.io/leaflet-providers/preview/
 var basemapOptions = {
-	blackAndWhite : L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {}),
-	imagery : L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {}),
-	watercolor : L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',{}),
-	osm : L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {})
+	'Black and White' : L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {}),
+	'Satalite' : L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {}),
+	'Watercolor' : L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',{}),
+	'Open Street Map' : L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {})
 };
 
 var albany	= [42.6525, -73.7572],
@@ -19,26 +19,30 @@ var albany	= [42.6525, -73.7572],
 var cities = [albany, buffalo, madison];
 
 function makeBigFoots(){
+	var style = {
+		color: 'darkorange',
+		weight: 1,
+		fillColor: 'yellow',
+		fillOpacity: 1
+	};
+
 	for (x in bigfoots) {
-		var sighting = L.circle(bigfoots[x].latlng);
+		var coords   = L.latLng([bigfoots[x].lat, bigfoots[x].lng]),
+			sighting = L.circleMarker(coords).setRadius(2).setStyle(style).addTo(map);
+
 		sighting['bigfootData'] = bigfoots[x];
-		sighting.setRadius(8);
-		sighting.setStyle({
-			'color':'red'
-		})
+
 		sighting.on('click',function(){
 			document.getElementById('bigfootDate').innerHTML = this.bigfootData.placemark;
 			document.getElementById('bigfootText').innerHTML = this.bigfootData.reportdesc;
 		});
-
-		sighting.addTo(map)
 	}
 }
 
 function createMap() {
 	map = L.map('map', {
 		center 	:	[39.7071, -97.3388],
-		layers  : 	[ basemapOptions.imagery ],
+		layers  : 	basemapOptions['Satalite'],
 		zoom 	: 	4
 	});
 
