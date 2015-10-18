0050a865e26dc4ea34671d4025e50fb04f760877
NathanMarq <NathanMarq@gmail.com>
2015-10-18 15:01:42 -0400

    doing some re-organizing and cleaning up

---
+++ exercises/steps/pre_step4/js/01_variables.js
@@ -0,0 +1,24 @@
+/*
+ *   Variable Definitions
+*/
+var myVar;
+
+var anotherVar,
+    yetAnotherVar;           // Chaining Variable Declarations
+
+    // console.log(myVar);   // >> undefined
+
+
+/*
+ *   Variable Types
+*/
+
+var stringVar = 'foo',
+    numberVar = 12345,
+    booleanVar = true,
+    undefinedVar;
+
+var arrayVar = [],
+    objectVar = {};
+
+var functionVar = function() { return true; };
